6f83a094b605dad8c58badb2691b5435ce7dfbb9
jun_zhao_dell_1 <jjz62@yahoo.com>
2019-09-26 16:31:59 -0400

    no comment

--- gitPushAll_v4.sh
+++ gitPushAll_v4.sh
@@ -3,13 +3,16 @@
 # $ sh /u/jjzALL/myGitALL/git_scripts/gitPushAll.sh
 #
 #
-#echo 'change to git-working dir'
-#cd /u/jjzALL/myGitALL
-pwd
 git add .
 #git commit -m 'no comment'
 git commit -am 'no comment'
-git push
+
+#git push
+# to work around the - fatal: No configured push destination 
+
+repo_url='https://github.com/zeleeo1/jjzGitRepo2.git'
+git push --set-upstream $repo_url master:master 
+
 # do a pause
 read -p "Press [Enter] key to close console........................"
 echo "this echo will never be seen, unless you have a really slow computer"
