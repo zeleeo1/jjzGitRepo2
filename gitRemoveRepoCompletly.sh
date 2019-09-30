2fb8e61f4eff5533f4fb9b4bd02ccb1e3d059bb0
jun_zhao_dell_1 <jjz62@yahoo.com>
2019-09-30 14:15:47 -0400

    no comment

---
+++ gitRemoveRepoCompletly.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# to run this script
+# $ sh /u/jjzALL/myGitALL/git_scripts/xyz.sh
+#
+# this file is still not working? 10.2019
+# with the 
+echo now showing remote versions...
+git remote -v
+echo
+echo
+echo now let you show the versions...
+repo_url='https://github.com/zeleeo1/jjzGitRepo2.git'
+git remote add origin $repo_url
+
+echo now showing remote versions...
+git remote -v
+echo
+echo
+#git pull origin master
+git remote remove origin
+git push -u origin master
+# do a pause
+read -p "Press [Enter] key to close console........................"
+echo "this echo will never be seen, unless you have a really slow computer"
