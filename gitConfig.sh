5b303a33f8520fe936a1b02a9ab877a7efd6fa9b
jun_zhao_dell_1 <jjz62@yahoo.com>
2019-10-01 19:28:52 -0400

    test - this is another very ok

---
+++ gitConfig.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# to run this script
+# $ sh /u/jjzALL/myGitALL/git_scripts/gitConfig.sh
+# every Git commit uses this information
+
+git config --global user.email "jjz62@yahoo.com"
+git config --global user.name "jun_zhao_dell_1"
+
+# above line tell you where are you commit from
+# do a pause
+read -p "Press [Enter] key to close console........................"
+echo "this echo will never be seen, unless you have a really slow computer"
