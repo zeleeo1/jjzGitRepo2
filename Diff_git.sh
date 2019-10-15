188c3818faac44b81f6e6d89503daf2304a40fe3
jun_zhao_dell_1 <jjz62@yahoo.com>
2019-10-15 17:03:57 -0400

    add-A - from pvt_b1

--- Diff_git.sh
+++
@@ -1,9 +0,0 @@
-#set -x 
-branchname=`git branch | grep -F '*' |  awk '{print $2}'`
-echo $branchname
-git fetch origin ${branchname}
-for file in `git status | awk '{if ($1 == "modified:") print $2;}'`
-do
-echo "PLEASE CHECK OUT GIT DIFF FOR "$file 
-git difftool  FETCH_HEAD $file ;
-done
